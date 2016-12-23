1cc3b40803636d75cd6b730f55ccc28cead739bb
Scott Reynen <scott.reynen@moveon.org>
2016-12-23 16:44:13 -0700

    non-working attempt to connect to API for data. need to figure out CORS

--- src/pages/sign-petition.js
+++ src/pages/sign-petition.js
@@ -1,12 +1,22 @@
 import Nav from '../components/nav.js';
 import Footer from '../components/footer.js';
 import Petition from '../components/petition.js';
+import 'whatwg-fetch';
 
 class SignPetition extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  componentWillMount() {
+    fetch('https://testpet2.moveon.org/petitions/economic-disparity-campaign')
+      .then(function(response) {
+        // console.log(response);
+        // this.setProp();
+      });
+  }
+
 
   render() {
     return (
