0a839a8c4ec49da6162b1b452a636d4b0d882e7f
Scott Reynen <scott.reynen@moveon.org>
2017-04-10 09:26:50 -0600

    update state description

--- src/store/defaultState.js
+++ src/store/defaultState.js
@@ -1,5 +1,6 @@
 const defaultState = {
-  'petitions': {}
+  'petitions': {}, // keys are petition slugs, values are petition objects
+  'signatureStatus': {} // keys are petition IDs, values are strings
 };
 
 export default defaultState;
