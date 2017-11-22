9616dd34f27e0325c10029d9bbd431cd5b34156e
Hawley Brett <AnneHawleyBrett@gmail.com>
2017-11-22 15:54:14 -0500

    further tests and adjustments for signature-list-item

--- src/components/signature-list-item.js
+++ src/components/signature-list-item.js
@@ -6,11 +6,11 @@ import { formatDate } from '../lib.js'
 const SignatureListItem = ({ user, created_date, number }) => {
   const { city, state, name } = user
   const location = ((!city) ? state : `${city}, ${state}`)
-  const fromLocation = ((location === '') ? '' : ` from ${location}`)
+  const fromLocation = ((location === '') ? '' : `from ${location}`)
   const date = new Date(created_date)
   return (
     <li className='signer'>
-      <span className='signer-number'>{number}</span>
+      <span className='signer-number'>{number}</span>{' '}
       <b>{name}</b> {fromLocation} signed this petition on {formatDate(date)}.
     </li>
   )
@@ -19,7 +19,7 @@ const SignatureListItem = ({ user, created_date, number }) => {
 SignatureListItem.propTypes = {
   user: PropTypes.shape({
     city: PropTypes.string,
-    state: PropTypes.string,
+    state: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
   }),
   created_date: PropTypes.number.isRequired,
--- test/components/singnature-list-item.js
+++ test/components/singnature-list-item.js
@@ -25,4 +25,26 @@ describe('<SignatureListItem />', () => {
 		expect(context.find('.signer-number').text())
 			.to.equal('1000');
 	});
+
+	it('renders name in bold', () => {
+		const context = shallow(<SignatureListItem user={user} created_date={created_date} number={1000} />);
+		expect(context.find('b').text())
+			.to.equal(user.name);
+	});
+
+	it('renders readable text with city and state', () => {
+		const context = shallow(<SignatureListItem user={user} created_date={created_date} number={1000} />);
+		expect(context.text())
+			.to.equal('1000 Ada Lovelace from Nottingham, England signed this petition on Dec 10, 1836.')
+	});
+
+	it('renders readable text with only state', () => {
+		const noCity = {
+			state: 'England',
+			name: 'Ada Lovelace',
+		}
+		const context = shallow(<SignatureListItem user={noCity} created_date={created_date} number={1000} />);
+		expect(context.text())
+			.to.equal('1000 Ada Lovelace from England signed this petition on Dec 10, 1836.')
+	});
 });
