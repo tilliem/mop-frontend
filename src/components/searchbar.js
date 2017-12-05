4322a9a8880faf9a0d170f566cd2ce44aaae7e52
shakalee14 <shakalee14@gmail.com>
2017-12-05 14:06:33 -0500

    Refactors state select to take text and style

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import StateSelect from './form/state-select'
 
+const smallStateSelectStyle = {
+  width: '20%',
+  marginRight: '5px'
+}
+
 const SearchBar = () => (
   <div className='container'>
     <div className='row'>
@@ -8,7 +13,7 @@ const SearchBar = () => (
         <form className='search' method='get' action='/find/'>
           <div className='search'>
             <input name='q' placeholder='Search Petitions' type='text' className='margin-right-1 ' />
-            <StateSelect />
+            <StateSelect selectText='All States' style={smallStateSelectStyle} />
             <button className='background-moveon-dark-blue margin-left-1' type='submit'>Search</button>
           </div>
         </form>
