5d795b7360cafb775098b4f450dde2e7bbb628d2
Scott Reynen <scott.reynen@moveon.org>
2017-12-15 16:54:52 -0700

    fix lint errors

--- src/components/sparkline.js
+++ src/components/sparkline.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines'
 
 const Sparkline = ({ data }) => (
   <div>
     {data.reduce((a, b) => a + b)}
     <Sparklines data={data} width={30} svgWidth={30} height={23} svgHeight={23}>
-      <SparklinesLine style={{ fill: "none" }} />
+      <SparklinesLine style={{ fill: 'none' }} />
       <SparklinesSpots />
     </Sparklines>
   </div>
--- src/pages/petition-report-page.js
+++ src/pages/petition-report-page.js
@@ -10,7 +10,7 @@ class PetitionReportPage extends React.Component {
 
   componentWillMount() {
     const self = this
-    const { petition, location, loadPetition } = this.props
+    const { petition, location, dispatchLoadPetition } = this.props
     const petitionId = location.query.petition_id
     petitionReportLoader().then((deps) => {
       self.petitionReport = deps.petitionReport.default
@@ -19,10 +19,9 @@ class PetitionReportPage extends React.Component {
 
     if (!petition) {
       if (petitionId) {
-        loadPetition(petitionId)
+        dispatchLoadPetition(petitionId)
       }
     }
-
   }
 
   render() {
@@ -30,28 +29,28 @@ class PetitionReportPage extends React.Component {
     // TODO: load signatureStats from API when data is available in API
     const signatureStats = {
       ever: {
-        all: [0,0,0,0,108269,46540,11962],
-        facebook: [0,0,0,0,393,466,107],
-        twitter: [0,0,0,0,281,187,30],
-        email: [0,0,0,0,1588,1336,273]
+        all: [0, 0, 0, 0, 108269, 46540, 11962],
+        facebook: [0, 0, 0, 0, 393, 466, 107],
+        twitter: [0, 0, 0, 0, 281, 187, 30],
+        email: [0, 0, 0, 0, 1588, 1336, 273]
       },
       hourly: {
-        all: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,12,11,9,10,9,16,16,19,21,8,10,12,14,16,14,14,10,18,13,15,14,6,15,13,16,7,5,15,18,9,14,15,13,13,8,11,7,6,10,11,15,11,11,14,9,16,15,8,15,12,11,12,16,17,12,7,15,20,13,8],
-        facebook: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
-        twitter: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,1,1],
-        email: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1]
+        all: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12, 11, 9, 10, 9, 16, 16, 19, 21, 8, 10, 12, 14, 16, 14, 14, 10, 18, 13, 15, 14, 6, 15, 13, 16, 7, 5, 15, 18, 9, 14, 15, 13, 13, 8, 11, 7, 6, 10, 11, 15, 11, 11, 14, 9, 16, 15, 8, 15, 12, 11, 12, 16, 17, 12, 7, 15, 20, 13, 8],
+        facebook: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        twitter: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 1],
+        email: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
       },
       daily: {
-        all: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,729,21],
-        facebook: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
-        twitter: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5],
-        email: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7]
+        all: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 729, 21],
+        facebook: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+        twitter: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
+        email: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7]
       },
       growth: {
-        all: [0,0,0,0,108269,46540,11962],
-        facebook: [0,0,0,0,393,466,107],
-        twitter: [0,0,0,0,281,187,30],
-        email: [0,0,0,0,1588,1336,273]
+        all: [0, 0, 0, 0, 108269, 46540, 11962],
+        facebook: [0, 0, 0, 0, 393, 466, 107],
+        twitter: [0, 0, 0, 0, 281, 187, 30],
+        email: [0, 0, 0, 0, 1588, 1336, 273]
       }
     }
     if (this.petitionReport && petition) {
@@ -64,19 +63,21 @@ class PetitionReportPage extends React.Component {
 }
 
 PetitionReportPage.propTypes = {
-  petition: PropTypes.oneOfType([PropTypes.object, PropTypes.bool])
+  petition: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  location: PropTypes.object,
+  dispatchLoadPetition: PropTypes.func
 }
 
 const mapStateToProps = (store, ownProps) => {
   const { petitionStore: { petitions } } = store
   const petition = petitions[ownProps.location.query.petition_id]
   return {
-   petition: (typeof petition === 'undefined') ? false : petition
+    petition: (typeof petition === 'undefined') ? false : petition
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  loadPetition: (petitionId) => dispatch(loadPetition(petitionId))
+  dispatchLoadPetition: (petitionId) => dispatch(loadPetition(petitionId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PetitionReportPage)
