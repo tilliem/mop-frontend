4a75a9b040db26df2fe21e1c0fa651abd115d786
Corey Frang <gnarf37@gmail.com>
2017-12-19 14:32:21 -0500

    Lint should include tests directory

---
+++ test/.eslintrc.js
@@ -0,0 +1,12 @@
+const parent = require('../.eslintrc')
+
+module.exports = Object.assign({}, parent, {
+  env: {
+    browser: true,
+    node: true,
+    mocha: true
+  },
+  rules: Object.assign({}, parent.rules, {
+    'no-unused-expressions': ['off']
+  })
+})
--- test/pages/home.js
+++ test/pages/home.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { expect } from 'chai';
+import React from 'react'
+import { expect } from 'chai'
 
-import { shallow } from 'enzyme';
+import { shallow } from 'enzyme'
 
-import Home from '../../src/pages/home';
+import Home from '../../src/pages/home'
 import BillBoard from '../../src/components/billboard'
 import SearchBar from '../../src/components/searchbar'
 import RecentVictoryList from '../../src/components/recentvictory.js'
 
 describe('<Home />', () => {
+  it('renders a billboard', () => {
+    const context = shallow(<Home />)
+    expect(context.find(BillBoard)).to.have.length(1)
+  })
 
-	it('renders a billboard', () => {
-		const context = shallow(<Home />);
-		expect(context.find(BillBoard)).to.have.length(1);
-	});
+  it('renders a searchbar', () => {
+    const context = shallow(<Home />)
+    expect(context.find(SearchBar)).to.have.length(1)
+  })
 
-	it('renders a searchbar', () => {
-		const context = shallow(<Home />);
-		expect(context.find(SearchBar)).to.have.length(1);
-	});
-
-	it('renders a recent victory list inside .front-content', () => {
-		const context = shallow(<Home />);
-		expect(context.find('.front-content').find(RecentVictoryList)).to.have.length(1);
-	});
-});
+  it('renders a recent victory list inside .front-content', () => {
+    const context = shallow(<Home />)
+    expect(context.find('.front-content').find(RecentVictoryList)).to.have.length(1)
+  })
+})
