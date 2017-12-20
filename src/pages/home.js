6f013b8fc66e1c8f990a94e6c83dd803428bdb24
Schuyler Duveen <sky@skyb.us>
2017-12-20 17:08:27 -0500

    add some basic home tests for an org

--- src/pages/home.js
+++ src/pages/home.js
@@ -20,7 +20,7 @@ const Home = ({ params, nav }) => {
 
       {isOrganization
        ? (
-        <div>
+        <div className='organization-header'>
           <h2>{orgData.organization}</h2>
           {orgData.description || `${orgData.organization} is a MoveOn MegaPartner, an invite-only program that lets a partner organization&#39;s members and activists set up their own MoveOn petitions in partnership with the original organization.`}
           <p className='pull-right'><a href='create_start.html' className='button background-moveon-bright-red'>Create a petition</a></p>
