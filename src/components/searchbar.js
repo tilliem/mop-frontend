55a324bf3b6d798a904107d2cc1eaf837c077f9e
shakalee14 <shakalee14@gmail.com>
2017-12-27 17:40:37 -0500

    Adds handlequery method to searchbar component

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -8,42 +8,67 @@ const smallStateSelectStyle = {
   marginRight: '5px'
 }
 
-const SearchBar = ({ size }) => {
+class SearchBar extends React.Component {
 
-  const longSearchBar = (
-    <div className="container">
-      <div className="row">
-        <div className="span7 control-group bump-top-1">
-          <form className="search" method="get" action="/#/find/">
-            <div className="search">
-              <input name="query" placeholder="Search Petitions" type="text" className="margin-right-1 " />
-              <StateSelect selectText="All States" style={smallStateSelectStyle} />
-              <button className="background-moveon-dark-blue margin-left-1" type="submit">Search</button>
-            </div>
-          </form>
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: ""
+    }
+  }
+  componentWillMount(){
+    const self = this
+    const { dispatch, query } = this.props
+  }
+
+  handleQuery(e){
+    e.preventDefault()
+    console.log('e:', e.target.value);
+    this.setState({
+      query: e.target.value
+    })
+    console.log('query', this.state.query);
+  }
+
+  render(){
+
+    const { size } = this.props
+
+    const longSearchBar = (
+      <div className="container">
+        <div className="row">
+          <div className="span7 control-group bump-top-1">
+            <form className="search" method="get" action="/#/find/">
+              <div className="search">
+                <input name="query" placeholder="Search Petitions" type="text" className="margin-right-1 " />
+                <StateSelect selectText="All States" style={smallStateSelectStyle} />
+                <button className="background-moveon-dark-blue margin-left-1" type="submit">Search</button>
+              </div>
+            </form>
+          </div>
+          <p className="lanky-header size-medium-small lh-24 bump-top-1">Search for petitions by any keyword.</p>
         </div>
-        <p className="lanky-header size-medium-small lh-24 bump-top-1">Search for petitions by any keyword.</p>
       </div>
-    </div>
-  )
+    )
 
-  const shortSearchBar = (
-    <div id="search-form-div">
-      <form className="form-vertical">
-        <div className="search" method="get" action="/#/find/">
-          <input name="query" type="text" id="search-box2" className="margin-top-0 margin-right-2"/>
-          <button type="submit" className="background-moveon-dark-blue">Search</button>
-        </div>
-        <div className="clear"></div>
-      </form>
-    </div>
-  )
+    const shortSearchBar = (
+      <div id="search-form-div">
+        <form className="form-vertical" onSubmit={this.handleQuery.bind(this)}>
+          <div className="search" method="get" action="/#/find/">
+            <input name="query" value={this.state.query} onChange={this.handleQuery.bind(this)} type="text" id="search-box2" className="margin-top-0 margin-right-2"/>
+            <button type="submit" className="background-moveon-dark-blue">Search</button>
+          </div>
+          <div className="clear"></div>
+        </form>
+      </div>
+    )
 
-  return (
-    <div>
-      {size === "long" ? longSearchBar : shortSearchBar }
-    </div>
-  )
+    return (
+      <div>
+        {size === "long" ? longSearchBar : shortSearchBar }
+      </div>
+    )
+  }
 }
 
 SearchBar.propTypes = {
