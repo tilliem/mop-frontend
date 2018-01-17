835c8a8d0caaad4db5643139306f61405ee94350
Schuyler Duveen <sky@skyb.us>
2018-01-17 16:06:56 -0500

    really disable capitalized-comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   extends: 'airbnb',
   parser: 'babel-eslint',
   rules: {
-    'semi': ['error', 'never'],
+    semi: ['error', 'never'],
     'comma-dangle': ['error', 'never'],
-    'capitalized-comments': ["error", 'never'],
+    'capitalized-comments': ['error', 'always', {
+      ignorePattern: '.*',
+      ignoreInlineComments: true
+    }],
     'no-underscore-dangle': ['off'],
     'no-alert': ['off'],
     'jsx-quotes': ['error', 'prefer-single'],
