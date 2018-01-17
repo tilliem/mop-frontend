1a5444e5b9582622706540a0564837fa5df2e857
Ann Lewis <the.annlewis@gmail.com>
2018-01-17 14:43:06 -0500

    fix tests

--- test/components/search-results.js
+++ test/components/search-results.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { expect } from 'chai'
 
 import { mount } from 'enzyme'
@@ -9,20 +10,22 @@ import SearchResults from '../../src/components/search-results'
 
 describe('<SearchResults />', () => {
   it('can render no results', () => {
-    const baseStore = createMockStore({ userStore: {}, petitionSearchStore: { searchResults: { _embed: [], _links: {}, count: '0', page_size: 0 } } })
     const props = { pageNumber: '1', query: '', pageSize: 0 }
-    const context = mount(<SearchResults {...props} store={baseStore} />)
+    const myComponent = <SearchResults {...props} params={{}} />
+    const baseStore = createMockStore({ userStore: {}, petitionSearchStore: { searchResults: { _embed: [], _links: {}, count: '0', page_size: 0 } } })
+    const context = mount(<Provider store={baseStore} children={myComponent} />)
     expect(context.find('#search-results').length).to.equal(1)
     expect(context.find('.result').length).to.equal(0)
+
   })
 
   it('can render a page of results', () => {
     const embed = [{ name: 'a' }, { name: 'a' }, { name: 'a' }, { name: 'a' }, { name: 'a' }]
     const links = { next: '', url: '' }
     const baseStore = createMockStore({ userStore: {}, petitionSearchStore: { searchResults: { _embed: embed, _links: links, count: '10', page_size: 5 } } })
-
     const props = { pageNumber: '1', query: '', pageSize: 5 }
-    const context = mount(<SearchResults {...props} store={baseStore} />)
+    const myComponent = <SearchResults {...props} params={{}} />
+    const context = mount(<Provider store={baseStore} children={myComponent} />)
     expect(context.find('#search-results').length).to.equal(1)
     expect(context.find('.result').length).to.equal(5)
   })
