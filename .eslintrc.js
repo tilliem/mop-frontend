d3d8206c7232e2e29c29967147bd12df304e87f8
Schuyler Duveen <sky@skyb.us>
2018-01-17 10:31:40 -0500

    add a devtips stub and remove capitalized-comments lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   extends: 'airbnb',
   parser: 'babel-eslint',
   rules: {
-    semi: ['error', 'never'],
+    'semi': ['error', 'never'],
     'comma-dangle': ['error', 'never'],
+    'capitalized-comments': ["error", 'never'],
     'no-underscore-dangle': ['off'],
     'no-alert': ['off'],
     'jsx-quotes': ['error', 'prefer-single'],
