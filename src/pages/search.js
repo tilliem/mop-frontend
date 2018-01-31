e118948ac5788fdeda9979a458bc992d60e18467
shakalee14 <shakalee14@gmail.com>
2018-01-31 13:25:36 -0500

    Adds updatedHeader method to render dynamic header based on search results

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,18 +5,33 @@ import { connect } from 'react-redux'
 import SearchBar from '../components/searchbar'
 import SearchResults from '../components/search-results'
 
-const SearchPage = ({ query, selectState, pageNumber }) =>
-  <div className='moveon-petitions container background-moveon-white bump-top-1'>
-    {<div className='row'>
-      <div className='span12'>
-        <h2 id='title' className='light'>Top petitions</h2>
-        <p>You can search for petitions by issue, title, author, target, city, state, or keyword &mdash; or check out <a href='/victories.html'>recent victories</a>.</p>
-        <SearchBar query={query} currentPage={pageNumber} isLong={false} />
-        <SearchResults query={query || ''} pageNumber={pageNumber || '1'} selectState={selectState || ''} />
-      </div>
-    </div>}
-  </div>
+const SearchPage = ({ query, selectState, pageNumber }) => {
 
+  const updatedHeader = (query, selectState) => {
+    if(query && !selectState){
+      return (<h2 id='title' className='light'> TOP PETITIONS MATCHING '{query}' </h2>)
+    } else if (query && selectState) {
+      return (<h2 id='title' className='light'> TOP PETITIONS MATCHING '{query}' FROM {selectState} </h2>)
+    } else if (selectState){
+      return (<h2 id='title' className='light'> TOP PETITIONS FROM {selectState} </h2>)
+    } else {
+      return (<h2 id='title' className='light'>Top petitions</h2>)
+    }
+  }
+
+  return(
+    <div className='moveon-petitions container background-moveon-white bump-top-1'>
+      {<div className='row'>
+        <div className='span12'>
+          {updatedHeader(query, selectState)}
+          <p>You can search for petitions by issue, title, author, target, city, state, or keyword &mdash; or check out <a href='/victories.html'>recent victories</a>.</p>
+          <SearchBar query={query} currentPage={pageNumber} isLong={false} />
+          <SearchResults query={query || ''} pageNumber={pageNumber || '1'} selectState={selectState || ''} />
+        </div>
+      </div>}
+    </div>
+  )
+}
 SearchPage.propTypes = {
   dispatch: PropTypes.func,
   params: PropTypes.object,
