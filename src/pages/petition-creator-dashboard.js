7199710a64a50dfc3ed3df1b7935afe9751584a8
Justin Parker <justin@jpark.tech>
2018-02-06 19:44:04 -0500

    Add PetitionOverview UI to user petition dashboard

--- src/pages/petition-creator-dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PetitionCreatorDashboard = () => (
-  <div className='moveon-petitions'>
-    <h2> Petition Creator Dashboard </h2>
-  </div>
-)
-
-export default PetitionCreatorDashboard
