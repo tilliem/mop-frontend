eb48692c7102a9dc3f4f41fb19b1800917dc594d
Scott Reynen <scott.reynen@moveon.org>
2018-02-10 09:01:17 -0700

    redirect to/from org sign URLs to match petition org

--- src/pages/sign-petition.js
+++ src/pages/sign-petition.js
@@ -6,12 +6,38 @@ import { connect } from 'react-redux'
 
 import Petition from '../components/petition.js'
 import { actions as petitionActions } from '../actions/petitionActions.js'
+import { appLocation } from '../routes.js'
 
 class SignPetition extends React.Component {
 
   componentWillMount() {
-    const { dispatch, params } = this.props
+    const { dispatch, params, petition } = this.props
     dispatch(petitionActions.loadPetition(params.petition_slug))
+    if (petition) {
+      this.checkOrgPathMatches(petition, params.organization)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { params, petition } = nextProps
+    if (petition) {
+      this.checkOrgPathMatches(petition, params.organization)
+    }
+  }
+
+  checkOrgPathMatches(petition, orgPath) {
+    const { creator } = petition._embedded
+    // Petition has org that doesn't match URL
+    if (creator.source && (creator.source != orgPath)) {
+      appLocation.push(`/${creator.source}/sign/${petition.name}`)
+      return false
+    }
+    // URL has org that doesn't match petition
+    if (orgPath && (orgPath != creator.source)) {
+      appLocation.push(`/sign/${petition.name}`)
+      return false
+    }
+    return true
   }
 
   render() {
