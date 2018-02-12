9697e675692e637fa1dcef155a209beeed748f15
Justin Parker <justin@jpark.tech>
2018-02-12 13:39:17 -0500

    Match style

--- src/components/form/target-select/legislator-autocomplete.js
+++ src/components/form/target-select/legislator-autocomplete.js
@@ -5,17 +5,33 @@ import Downshift from 'downshift'
 import Config from '../../../config.js'
 
 const container = {
+  marginTop: '-3px',
+  padding: '2px',
   borderRadius: '4px',
   position: 'absolute',
   border: '1px solid #aaaaaa',
-  background: '#fff'
+  background: '#fff',
+  backgroundImage: 'url(http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/base/images/ui-bg_flat_75_ffffff_40x100.png) 50% 50% repeat-x',
+  color: '#222222',
+  fontFamily: 'Verdana,Arial,sans-serif',
+  fontSize: '1.1em',
+  cursor: 'default'
 }
 
 const hover = {
   border: '1px solid #999999',
-  background: '#dadada',
-  borderRadius: '4px'
+  background: '#dadada url(http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/base/images/ui-bg_glass_75_dadada_1x400.png) 50% 50% repeat-x',
+  color: '#212121',
+  margin: '-1px'
 }
+
+const itemStyle = {
+  borderRadius: '4px',
+  padding: '.2em .4em',
+  lineHeight: 1.5
+}
+
+const itemHover = { ...itemStyle, ...hover }
 
 const filter = (items, inputValue = '') =>
   items.filter(i => i.name.toLowerCase().includes(inputValue.toLowerCase()))
@@ -78,7 +94,7 @@ class LegislatorAutocomplete extends React.Component {
                   <div
                     {...getItemProps({ item })}
                     key={item.value}
-                    style={highlightedIndex === index ? hover : {}}
+                    style={highlightedIndex === index ? itemHover : itemStyle}
                   >
                     {item.label}
                   </div>
