37cb38c9aa61ae52edd8fecae72479b34c091320
Justin Parker <justin@jpark.tech>
2018-02-14 14:05:29 -0500

    Extract stateless Search component

--- src/containers/search.js
+++ src/containers/search.js
@@ -2,41 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import SearchBar from '../containers/searchbar'
-import SearchResults from '../components/search-results'
-import StateCheckBox from '../components/state-check-box'
-import { getStateFullName } from '../components/state-abbrev.js'
+import SearchComponent from 'LegacyTheme/search'
 
-const SearchPage = ({ query, selectState, pageNumber }) => {
-  const updatedHeader = (q, s) => {
-    const stateFullName = getStateFullName(s)
-    if (q && !s) {
-      return (<h2 id='title' className='light'> TOP PETITIONS MATCHING '{q}' </h2>)
-    } else if (q && s) {
-      return (<h2 id='title' className='light'> TOP PETITIONS MATCHING '{q}' FROM {stateFullName} </h2>)
-    } else if (s) {
-      return (<h2 id='title' className='light'> TOP PETITIONS FROM {stateFullName} </h2>)
-    }
-    return (<h2 id='title' className='light'>Top petitions</h2>)
-  }
+const Search = ({ query, selectState, pageNumber }) => (
+  <SearchComponent
+    query={query}
+    pageNumber={pageNumber}
+    selectState={selectState}
+  />
+)
 
-  return (
-    <div className='moveon-petitions container background-moveon-white bump-top-1'>
-      {<div className='row'>
-        <div className='span12'>
-          {updatedHeader(query, selectState)}
-          <p>You can search for petitions by issue, title, author, target, city, state, or keyword &mdash; or check out <a href='/victories.html'>recent victories</a>.</p>
-          <div id='search-form-div' className='search-page'>
-            <SearchBar query={query} currentPage={pageNumber} isLong={false} />
-            {selectState ? <StateCheckBox selectState={selectState} query={query} /> : ''}
-          </div>
-          <SearchResults query={query || ''} pageNumber={pageNumber || '1'} selectState={selectState || ''} />
-        </div>
-      </div>}
-    </div>
-  )
-}
-SearchPage.propTypes = {
+Search.propTypes = {
   dispatch: PropTypes.func,
   params: PropTypes.object,
   query: PropTypes.string,
@@ -62,4 +38,4 @@ function mapStateToProps(store, ownProps) {
   }
 }
 
-export default connect(mapStateToProps)(SearchPage)
+export default connect(mapStateToProps)(Search)
