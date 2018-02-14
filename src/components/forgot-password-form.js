448e8058e992b3c91c0b00b21db199284b738b8e
Justin Parker <justin@jpark.tech>
2018-02-14 10:57:17 -0500

    Move forgot-password-form methods to forgot-password container

--- src/components/forgot-password-form.js
+++ src/components/forgot-password-form.js
@@ -1,125 +1,64 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class ForgotPasswordForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      presubmitErrors: [],
-      submitted: false
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
+const ThanksMessage = (
+  <div>
+    An email has been sent containing a link you can use to reset your MoveOn
+    Petitions password. <b>Thank you for using MoveOn’s petition website!</b>
+  </div>
+)
 
-  isValidEmail(email) {
-    const regex = /.+@.+\..+/ // Forgiving email regex
-    return regex.test(email)
-  }
-
-  /**
-   * Validates the form for client side errors.
-   * If valid returns true otherwise false.
-   * If errors it will update the local state `presubmitErrors`
-   * @returns {boolean}
-   */
-  validateForm() {
-    const errors = []
-    if (!this.isValidEmail(this.email.value)) {
-      if (!this.email.value.trim().length) {
-        errors.push({ message: 'Missing required entry for the Email field.' })
-      } else {
-        errors.push({ message: 'Invalid entry for the Email field.' })
-      }
-    }
-    if (errors.length) {
-      this.setState({ presubmitErrors: errors })
-    }
-    return !errors.length
-  }
-
-  handleSubmit(event) {
-    event.preventDefault()
-    if (this.validateForm()) {
-      this.props.onSubmit(this.email.value).then(() => {
-        this.setState({ submitted: true })
-      })
-    }
-  }
-
-  /**
-   * Get the current errors as a jsx array.
-   * @returns {Array} an jsx array of errors
-   */
-  errorList() {
-    return this.state.presubmitErrors.map((error, idx) => <li key={idx}>{error.message}</li>)
-  }
-
-  renderThanks() {
-    return (
-      <div>
-        An email has been sent containing a link you can use to reset your
-        MoveOn Petitions password.{' '}
-        <b>Thank you for using MoveOn’s petition website!</b>
-      </div>
-    )
-  }
-
-  renderForm() {
-    return (
-      <div>
-        <p>
-          Enter your email address here and we’ll send you a link you can use to
-          change your password.
-        </p>
-        <ul className='errors'>{this.errorList()}</ul>
-
-        <form
-          method='POST'
-          onSubmit={this.handleSubmit}
-          className='form-horizontal'
-        >
-          <div className='control-group'>
-            <label className='control-label' htmlFor='inputEmail'>
-              Email:
-            </label>
-            <div className='controls'>
-              <input
-                ref={email => (this.email = email)}
-                name='email'
-                className='percent-80'
-                type='text'
-                placeholder='Email'
-              />
-            </div>
+const ForgotPasswordForm = ({ errorList, handleSubmit, getEmailRef, submitted }) => {
+  const Form = (
+    <div>
+      <p>
+        Enter your email address here and we’ll send you a link you can use to
+        change your password.
+      </p>
+      <ul className='errors'>{errorList && errorList()}</ul>
+      <form method='POST' onSubmit={handleSubmit} className='form-horizontal'>
+        <div className='control-group'>
+          <label className='control-label' htmlFor='inputEmail'>
+            Email:
+          </label>
+          <div className='controls'>
+            <input
+              ref={getEmailRef}
+              name='email'
+              className='percent-80'
+              type='text'
+              placeholder='Email'
+            />
           </div>
-          <div className='control-group'>
-            <div className='controls'>
-              <input type='submit' value='Submit' className='button' />
-            </div>
+        </div>
+        <div className='control-group'>
+          <div className='controls'>
+            <input type='submit' value='Submit' className='button' />
           </div>
-        </form>
-      </div>
-    )
-  }
+        </div>
+      </form>
+    </div>
+  )
 
-  render() {
-    return (
-      <div className='container'>
-        <div className='row'>
-          <div className='span6 offset3'>
-            <div className='well'>
-              <h2 className='legend'>Forgot Password</h2>
-              {this.state.submitted ? this.renderThanks() : this.renderForm()}
-            </div>
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='span6 offset3'>
+          <div className='well'>
+            <h2 className='legend'>Forgot Password</h2>
+            {submitted ? ThanksMessage : Form}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 ForgotPasswordForm.propTypes = {
-  onSubmit: PropTypes.func
+  handleSubmit: PropTypes.func,
+  submitted: PropTypes.bool,
+  errorList: PropTypes.func,
+  getEmailRef: PropTypes.func
 }
 
 export default ForgotPasswordForm
--- src/containers/forgot-password.js
+++ src/containers/forgot-password.js
@@ -3,20 +3,71 @@ import React from 'react'
 import ForgotPasswordForm from '../components/forgot-password-form'
 import { actions as accountActions } from '../actions/accountActions'
 
+import { isValidEmail } from '../lib'
+
 class ForgotPassword extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      presubmitErrors: [],
+      submitted: false
+    }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.errorList = this.errorList.bind(this)
+    this.validateForm = this.validateForm.bind(this)
   }
 
-  handleSubmit(email) {
-    return accountActions.forgotPassword(email)
+  /**
+   * Validates the form for client side errors.
+   * If valid returns true otherwise false.
+   * If errors it will update the local state `presubmitErrors`
+   * @returns {boolean}
+   */
+  validateForm() {
+    const errors = []
+    if (!isValidEmail(this.email.value)) {
+      if (!this.email.value.trim().length) {
+        errors.push({ message: 'Missing required entry for the Email field.' })
+      } else {
+        errors.push({ message: 'Invalid entry for the Email field.' })
+      }
+    }
+    if (errors.length) {
+      this.setState({ presubmitErrors: errors })
+    }
+    return !errors.length
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    if (this.validateForm()) {
+      accountActions.forgotPassword(this.email.value).then(() => {
+        this.setState({ submitted: true })
+      })
+    }
+  }
+
+  /**
+   * Get the current errors as a jsx array.
+   * @returns {Array} an jsx array of errors
+   */
+  errorList() {
+    return this.state.presubmitErrors.map((error, idx) => (
+      <li key={idx}>{error.message}</li>
+    ))
   }
 
   render() {
     return (
       <div className='moveon-petitions'>
-        <ForgotPasswordForm onSubmit={this.handleSubmit} />
+        <ForgotPasswordForm
+          handleSubmit={this.handleSubmit}
+          submitted={this.state.submitted}
+          errorList={this.errorList}
+          getEmailRef={input => {
+            this.email = input
+          }}
+        />
       </div>
     )
   }
