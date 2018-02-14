9a7bcfc24f8b9418ffa9447ca7eb20ee6cc8e593
Justin Parker <justin@jpark.tech>
2018-02-14 14:29:42 -0500

    Extract stateless SearchResults component

---
+++ src/components/theme-legacy/search-results.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import SearchResultPagination from '../search-result-pagination'
+
+const SearchResults = ({
+  results,
+  resultCount,
+  pageSize,
+  currentPage,
+  searchNavLinks,
+  query,
+  setResultsRef
+}) => {
+  const resultsList = results.map((result, index) => (
+    <div className='result search-page' key={index}>
+      <div className='result-text'>
+        <p className='result-name'>
+          <a
+            className='size-medium-large font-heavy'
+            href={`https://pac.petitions.moveon.org/sign/${
+              result.short_name
+            }/?source=search`}
+          >
+            {result.name}
+          </a>
+        </p>
+        <p className='size-small'>
+          https://pac.petitions.moveon.org/sign/{result.short_name}/
+        </p>
+        <p className='size-medium'>{result.blurb}...</p>
+      </div>
+    </div>
+  ))
+
+  return (
+    <div
+      id='search-results'
+      ref={setResultsRef}
+    >
+      {resultsList}
+      <SearchResultPagination
+        resultCount={resultCount}
+        pageSize={pageSize}
+        currentPage={currentPage}
+        searchNavLinks={searchNavLinks}
+        query={query}
+      />
+    </div>
+  )
+}
+
+SearchResults.propTypes = {
+  results: PropTypes.array,
+  resultCount: PropTypes.number,
+  currentPage: PropTypes.number,
+  query: PropTypes.string,
+  searchNavLinks: PropTypes.array,
+  pageSize: PropTypes.number,
+  setResultsRef: PropTypes.func
+}
+
+export default SearchResults
