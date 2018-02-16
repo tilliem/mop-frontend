9f21269670f1132ea1b3c30d2ba0d887b672ae31
Hawley Brett <AnneHawleyBrett@gmail.com>
2018-02-16 14:40:57 -0500

    test for fetch top petitions success

--- test/reducers/petition.js
+++ test/reducers/petition.js
@@ -3,6 +3,8 @@ import reducer from '../../src/reducers'
 
 import { actionTypes as accountActionTypes } from '../../src/actions/accountActions.js'
 import sampleUserPetions from '../../local/api/v1/user/petitions.json'
+import sampleTopPetitions from '../../local/api/v1/top-petitions.json'
+import { actionTypes as petitionActionTypes } from '../../src/actions/petitionActions.js'
 
 const defaultState = reducer(undefined, {})
 
@@ -14,6 +16,21 @@ describe('petition reducer', () => {
     }
 
     const state = reducer(defaultState, action)
+    expect(Object.keys(state.petitionStore.petitions)).to.deep.equal(['95983', 'outkast'])
+  })
+
+  it('adds top petitions to state when FETCH_TOP_PETITIONS_SUCCESS', () => {
+    const action = {
+      type: petitionActionTypes.FETCH_TOP_PETITIONS_SUCCESS,
+      petitions: sampleTopPetitions._embedded,
+      topPetitionsKey: 'test1'
+    }
+
+    const state = reducer(defaultState, action)
+
+    expect(state.petitionStore.topPetitions)
+    	.to.have.property('test1')
+    	.to.deep.equal([95983])
     expect(Object.keys(state.petitionStore.petitions)).to.deep.equal(['95983', 'outkast'])
   })
 })
