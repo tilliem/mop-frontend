dad9ca586aab00ada52995b01638487f108f3450
Hawley Brett <AnneHawleyBrett@gmail.com>
2018-02-23 11:08:31 -0500

    test for petition signature success action

--- test/reducers/petition.js
+++ test/reducers/petition.js
@@ -4,12 +4,13 @@ import reducer from '../../src/reducers'
 import { actionTypes as accountActionTypes } from '../../src/actions/accountActions.js'
 import sampleUserPetions from '../../local/api/v1/user/petitions.json'
 import sampleTopPetitions from '../../local/api/v1/top-petitions.json'
+import samplePetition from '../../local/api/v1/petitions/outkast.json'
 import { actionTypes as petitionActionTypes } from '../../src/actions/petitionActions.js'
 
 const defaultState = reducer(undefined, {})
 
 describe('petition reducer', () => {
-  it('adds petitions to state when FETCH_USER_PETITIONS_SUCCESS', () => {
+  it('adds users petitions to state when FETCH_USER_PETITIONS_SUCCESS', () => {
     const action = {
       type: accountActionTypes.FETCH_USER_PETITIONS_SUCCESS,
       petitions: sampleUserPetions._embedded
@@ -32,5 +33,19 @@ describe('petition reducer', () => {
       .to.have.property('test1')
       .to.deep.equal([95983])
     expect(Object.keys(state.petitionStore.petitions)).to.deep.equal(['95983', 'outkast'])
+  })
+
+  it('adds signature to petitions state when PETITION_SIGNATURE_SUCCESS', () => {
+    const action = {
+      type: petitionActionTypes.PETITION_SIGNATURE_SUCCESS,
+      signatureStatus: 'success',
+      petition: samplePetition
+    }
+
+    const state = reducer(defaultState, action)
+
+    expect(state.petitionStore.signatureStatus)
+      .to.have.property('95983')
+      .to.deep.equal('success')
   })
 })
