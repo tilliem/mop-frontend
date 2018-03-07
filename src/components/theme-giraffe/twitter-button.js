3539e5c7f5328a7d5f2d6ddf3ae37f5437735464
Justin Parker <justin@jpark.tech>
2018-03-07 12:39:08 -0500

    Giraffe thanks fix tweet

--- src/components/theme-giraffe/twitter-button.js
+++ src/components/theme-giraffe/twitter-button.js
@@ -4,24 +4,15 @@ import PropTypes from 'prop-types'
 import { withTwitter } from '../../containers/hoc-twitter'
 import TwitterSvg from 'GiraffeUI/svgs/twitter.svg'
 
-const TwitterButton = ({ onClick, tweet, setTweetRef }) => (
+const TwitterButton = ({ onClick }) => (
   <a href='#' className='mo-btn share-modal__link' onClick={onClick}>
     <TwitterSvg />
     Twitter
-    <textarea
-      className='hidden'
-      id='tweet_text'
-      defaultValue={tweet}
-      ref={setTweetRef}
-      readOnly
-    />
   </a>
 )
 
 TwitterButton.propTypes = {
-  onClick: PropTypes.func,
-  tweet: PropTypes.string,
-  setTweetRef: PropTypes.func
+  onClick: PropTypes.func
 }
 
 export default withTwitter(TwitterButton)
