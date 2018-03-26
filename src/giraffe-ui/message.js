f87952947ab2a5ba537a6cfd04fade73f32e40e0
Justin Parker <justin@jpark.tech>
2018-03-26 18:29:49 -0400

    Use correct classname for message background color

--- src/giraffe-ui/message.js
+++ src/giraffe-ui/message.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const Message = ({ children, action, color }) => (
-  <div className={`message message--${color} col-12 mb-4 mb-md-5 p-3`}>
+  <div className={`message bg-${color} col-12 mb-4 mb-md-5 p-3`}>
     <div className='row'>
       <div className='col'>{children}</div>
       {action && <div className='col-md-auto mt-3 mt-md-0'>{action}</div>}
