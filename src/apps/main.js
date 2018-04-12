d0f6f85bc71362f66f7203a5f8501b0fd3b4a1c0
Justin Parker <justin@jpark.tech>
2018-04-12 14:21:11 -0400

    Wait for document to load to render

--- src/apps/main.js
+++ src/apps/main.js
@@ -8,10 +8,16 @@ import { routes } from '../routes.js'
 // See reducers/index.js for initialState (and all possible transient states
 const store = configureStore()
 
-// NOTE: this requires the javascript to be loaded at the bottom of the page
-// or at least after the id="root" element
+function run() {
+  ReactDOM.render(
+    <Provider store={store} children={routes(store)} />,
+    document.getElementById('root')
+  )
+}
 
-ReactDOM.render(
-  <Provider store={store} children={routes(store)} />,
-  document.getElementById('root')
-)
+const loadedStates = ['complete', 'loaded', 'interactive']
+if (loadedStates.includes(document.readyState) && document.body) {
+  run()
+} else {
+  window.addEventListener('DOMContentLoaded', run, false)
+}
